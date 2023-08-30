6af2ca090bada1f2b2f6e31dd30f978dc284b5f9
Grigorii <nyarumilian@gmail.com>
2023-08-30 21:38:59 +0400

    feat: impl math

--- main.go
+++ main.go
@@ -16,9 +16,7 @@ import (
 )
 
 const (
-	UniswapFactoryAddr  = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
-	UniswapRouter02Addr = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-	UniswapPairAddr     = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852"
+	UniswapFactoryAddr = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
 )
 
 func weiToDecimal(wei *big.Int, delim *big.Float) *big.Float {
@@ -35,8 +33,17 @@ func etherToWei(eth *big.Float) *big.Int {
 	return wei
 }
 
+// getAmountOut reimplements math from uniswap contract
+func getAmountOut(inputAmount int, reserve0, reserve1 *big.Int) *big.Float {
+	amountInWithFee := inputAmount * 997
+	numerator := new(big.Int).Mul(big.NewInt(int64(amountInWithFee)), reserve1)
+	preDenominator := new(big.Int).Mul(reserve0, big.NewInt(1000))
+	denominator := new(big.Int).Add(preDenominator, big.NewInt(int64(amountInWithFee)))
+
+	return new(big.Float).Quo(new(big.Float).SetInt(numerator), new(big.Float).SetInt(denominator))
+}
+
 func main() {
-	uniswapRouter02Addr := flag.String("router-addr", UniswapRouter02Addr, "Put address of uniswap router v02")
 	uniswapFactoryAddr := flag.String("factory-addr", UniswapFactoryAddr, "Put address of uniswap factory")
 	inputToken := flag.String("input-token", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", "Put address of input token (default: WETH)")
 	outputToken := flag.String("output-token", "0xdac17f958d2ee523a2206206994597c13d831ec7", "Put address of output token (default: USDT)")
@@ -49,11 +56,6 @@ func main() {
 	}
 
 	uniswapV2Factory, err := store.NewUniswapv2factory(common.HexToAddress(*uniswapFactoryAddr), client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	uniswapV2Router, err := store.NewUniswapv2router02(common.HexToAddress(*uniswapRouter02Addr), client)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -77,14 +79,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	amountsOut, err := uniswapV2Router.GetAmountOut(
-		&bind.CallOpts{},
-		etherToWei(big.NewFloat(float64(*inputAmount))),
-		reserves.Reserve0, reserves.Reserve1,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(amountsOut)
+	fmt.Println(etherToWei(getAmountOut(*inputAmount, reserves.Reserve0, reserves.Reserve1)))
 }
